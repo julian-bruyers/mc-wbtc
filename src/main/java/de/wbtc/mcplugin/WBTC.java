108db881584820555170caaee5097e8851eb8504
Julian Bruyers <julian.bruyers@gmail.com>
2024-12-28 20:03:04 +0100

    > Initial implementation of the info command executor.

--- src/main/java/de/wbtc/mcplugin/WBTC.java
+++ src/main/java/de/wbtc/mcplugin/WBTC.java
@@ -1,17 +1,36 @@
 package de.wbtc.mcplugin;
 
+//Java
+import java.util.Objects;
+
+//Commands
+import de.wbtc.mcplugin.commands.InfoCMD;
+
+//Bukkit
 import org.bukkit.plugin.java.JavaPlugin;
 
+
+/**
+ * The main class of the WBTC plugin.
+ *
+ * @author Julian Bruyers
+ */
 public final class WBTC extends JavaPlugin {
 
+    /**
+     * Main method called, when the plugin is enabled/loaded by the server.
+     */
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getCommand("wbtc").setExecutor(new InfoCMD());
 
     }
 
+    /**
+     * Main method called, when the plugin is disabled/unloaded by the server.
+     */
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
