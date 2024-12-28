41456d810c1adabd9ff6b673754816232de178b4
Julian Bruyers <julian.bruyers@gmail.com>
2024-12-28 20:02:38 +0100

    > Adding debug mode to the Settings.java

--- src/main/java/de/wbtc/mcplugin/Settings.java
+++ src/main/java/de/wbtc/mcplugin/Settings.java
@@ -1,6 +1,7 @@
 package de.wbtc.mcplugin;
 
 public final class Settings {
+    public static boolean DEBUG = false;
     public static String PLUGIN_NAME = "WBTC";
     public static String VERSION = "1.0.0-BETA";
     public static String AUTHOR = "Julian Bruyers";
