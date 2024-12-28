a029a832bc93192b39d3950ee4d5ac5d4e6833de
Julian Bruyers <julian.bruyers@gmail.com>
2024-12-28 18:07:50 +0100

    > Initial Commit

---
+++ src/main/java/de/wbtc/mcplugin/WBTC.java
@@ -0,0 +1,17 @@
+package de.wbtc.mcplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WBTC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
